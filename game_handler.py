bcfb3d85b7b7e10505fd7c8e3998831c736fc0dc
Fengalf <14805375+Fengalf@users.noreply.github.com>
2023-11-02 21:37:40 +0100

    added character control and creation

--- game_handler.py
+++ game_handler.py
@@ -21,6 +21,19 @@ class GameStatusHandler():
         self.text_color = 'white'
         self.character_list = []
         self.characters_at_start = 3
+        self.angle = 90
+        self.headings = {
+            "east": 0,
+            "north": 90,
+            "west": 180,
+            "south": 270
+        }
+        self.opposite_headings = {
+            "east": "west",
+            "north": "south",
+            "west": "east",
+            "south": "north"
+        }
 
     def level_up_speed(self):
         if self.score == self.threshold:
@@ -65,3 +78,74 @@ class GameStatusHandler():
             stretch_wid=self.character_size, stretch_len=self.character_size)
         self.character.speed(self.character_pace)
         self.character_list.append(self.character)
+
+    def move_character(self, character):
+        """
+            Moves exactly one character, each time this function is called.
+        """
+
+    def move_all_characters_but_first(self, start_backwards=True):
+        """
+            Moves all characters in the status handler's character_list,
+            into direction of the next character's position.
+            By default the list is worked last to first.
+            Set start_backwards to false, to reverse the movement order.
+        """
+        if start_backwards:
+            range_steps = -1
+        else:
+            range_steps = 1
+
+        for index in range(len(self.character_list)-1, 0, range_steps):
+            next_position = self.character_list[index-1].pos()
+            self.character_list[index].setpos(next_position)
+
+    def move_first_character(self):
+        """
+            Moves only the first character in the character_list.
+        """
+        self.character_list[0].forward(self.character_pace)
+
+    def turn_first_character_clockwise(self,):
+        """
+            Changes only the first characters heading clockwise.
+        """
+        self.character_list[0].right(self.angle)
+
+    def turn_first_character_counter_clockwise(self):
+        """
+            Changes only the first characters heading clockwise.
+        """
+        self.character_list[0].left(self.angle)
+
+    def turn_first_character_up(self):
+        """
+            Changes only the first characters heading upwards.
+        """
+        if self.character_list[0].heading() != self.headings["north"] \
+                and self.character_list[0].heading() != self.headings["south"]:
+            self.character_list[0].setheading(self.headings["north"])
+
+    def turn_first_character_down(self):
+        """
+            Changes only the first characters heading downwards.
+        """
+        if self.character_list[0].heading() != self.headings["south"] \
+                and self.character_list[0].heading() != self.headings["north"]:
+            self.character_list[0].setheading(self.headings["south"])
+
+    def turn_first_character_left(self):
+        """
+            Changes only the first characters heading left.
+        """
+        if self.character_list[0].heading() != self.headings["west"] \
+                and self.character_list[0].heading() != self.headings["east"]:
+            self.character_list[0].setheading(self.headings["west"])
+
+    def turn_first_character_right(self):
+        """
+            Changes only the first characters heading right.
+        """
+        if self.character_list[0].heading() != self.headings["east"] \
+                and self.character_list[0].heading() != self.headings["west"]:
+            self.character_list[0].setheading(self.headings["east"])
